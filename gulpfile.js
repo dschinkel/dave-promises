98162bb36ef1402958d4970d07a615cdd04779e3
dschinkel <dschinkel@gmail.com>
2016-04-20 15:05:39 -0500

    added babel & some cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,21 @@
-'use strict';
+"use strict";
+require('babel-core/register');
 
 var gulp = require('gulp'),
     mocha = require('gulp-mocha');
 
 var config = {
-    core: {
-        src: '*.js'
-    },
     test:{
-        src: {
-            unit:'test/unit/**/*-test.js'
-        },
+        unit:'test/unit/**/*-test.js',
         mocha: {
             reporter: 'spec'
         }
     }
 };
 
-gulp.task('default', ['local'], function(){
-});
-
-gulp.task('local',['mocha-unit'], function() {
-    gulp.watch('**/*.js', ['mocha-unit']);
-});
-
 gulp.task('test', function() {
     process.env.PORT = 8000;
-    return gulp.src([config.test.src.unit], { read: false })
+    return gulp.src([config.test.unit], { read: false })
         .pipe(mocha({
             reporter: config.test.mocha.reporter,
             ui: 'bdd'
