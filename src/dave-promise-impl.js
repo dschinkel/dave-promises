87b94955af87e9e33a4fe2bf11439dfc51f192ca
dschinkel <dschinkel@gmail.com>
2016-04-23 17:10:07 -0500

    checking in red test, then working on making it pass next

---
+++ src/dave-promise-impl.js
@@ -0,0 +1,5 @@
+
+
+//function DavePromise(promise){
+//
+//}
--- test/unit/promise-test.js
+++ test/unit/promise-test.js
@@ -1,12 +1,13 @@
 'use strict';
 
+import {DavePromise} from '../../DavePromiseImpl'
 let chai = require('chai'),
     expect = chai.expect();
 
 describe('Promise', () => {
 
     it('should callback when resolve is invoked', () => {
-        const DavePromise = new Promise(resolve => {
+        const DavePromise = new DavePromiseImpl(resolve => {
             resolve('hi!');
             }).then(value => {
                 expect(value).to.equal('hi!');
