e5f4d5ff369528a0591847403cdfde351cbad5af
dschinkel <dschinkel@gmail.com>
2016-04-23 16:50:06 -0500

    uncommented original test

--- test/unit/promise-test.js
+++ test/unit/promise-test.js
@@ -6,12 +6,11 @@ let chai = require('chai'),
 describe('Promise', () => {
 
     it('should callback when resolve is invoked', () => {
-        //new Promise(resolve => {
-        //    resolve('hi!');
-        //    }).then(value => {
-        //        expect(value).to.equal('hi!');
-        //    });
-        expect(false).to.equal(false);
+        new Promise(resolve => {
+            resolve('hi!');
+            }).then(value => {
+                expect(value).to.equal('hi!');
+            });
     });
 
 });
