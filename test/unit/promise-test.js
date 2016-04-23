7258eca8675d2d8379a9f6831383c9e16711a6d0
dschinkel <dschinkel@gmail.com>
2016-04-23 17:20:45 -0500

    create constant to be referenced from tests so that I can swap out the implementation for DavePromise

--- test/unit/promise-test.js
+++ test/unit/promise-test.js
@@ -1,16 +1,16 @@
-'use strict';
-
-//import {DavePromise} from '../../DavePromiseImpl'
+import {DavePromiseImpl} from '../../DavePromiseImpl'
 import {expect} from 'chai';
+
+const DavePromise = DavePromiseImpl;
 
 describe('Promise', () => {
 
     it('should callback when resolve is invoked', () => {
-        //const DavePromise = new DavePromiseImpl(resolve => {
-        //    resolve('hi!');
-        //    }).then(value => {
-        //        expect(value).to.equal('hi!');
-        //    });
+        DavePromise = new DavePromiseImpl(resolve => {
+            resolve('hi!');
+            }).then(value => {
+                expect(value).to.equal('hi!');
+            });
     });
 
 });
