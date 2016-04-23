05b3e9b9f6847399702719f8e711d5dd3565257a
dschinkel <dschinkel@gmail.com>
2016-04-23 17:14:10 -0500

    changed to import

--- test/unit/promise-test.js
+++ test/unit/promise-test.js
@@ -1,17 +1,16 @@
 'use strict';
 
-import {DavePromise} from '../../DavePromiseImpl'
-let chai = require('chai'),
-    expect = chai.expect();
+//import {DavePromise} from '../../DavePromiseImpl'
+import {expect} from 'chai';
 
 describe('Promise', () => {
 
     it('should callback when resolve is invoked', () => {
-        const DavePromise = new DavePromiseImpl(resolve => {
-            resolve('hi!');
-            }).then(value => {
-                expect(value).to.equal('hi!');
-            });
+        //const DavePromise = new DavePromiseImpl(resolve => {
+        //    resolve('hi!');
+        //    }).then(value => {
+        //        expect(value).to.equal('hi!');
+        //    });
     });
 
 });
