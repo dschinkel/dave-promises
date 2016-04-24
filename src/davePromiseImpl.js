8f3747193038e354d4631131e7ac394c3eaf0bdf
dschinkel <dschinkel@gmail.com>
2016-04-23 20:47:25 -0500

    progress but not passing yet

--- src/davePromiseImpl.js
+++ src/davePromiseImpl.js
@@ -1,9 +1,33 @@
 
 
-export default function(callback){
-    //function(resolve) { resolve('hi') }
-    var resolveIt = (value) => {return value};
 
-    return callback((resolveIt));
+var result;
+var resolved = false;
+
+const impl = function(callback){
+
+    callback(function(resolvedValue){
+        result = resolvedValue;
+    });
+
+    //function(resolve) { resolve('hi') }
+    /*
+        function((resolve) => {return value}){
+
+            resolve('hi');
+        }
+     */
+
+    if(result){
+        resolved = true;
+    }
 };
 
+impl.then = function(handler){
+    if(resolved){
+        return handler(result);
+    }
+
+}
+
+export default impl;
