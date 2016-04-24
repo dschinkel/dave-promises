0253c2d0848c8a218985d4cfa70dbede094a8efd
dschinkel <dschinkel@gmail.com>
2016-04-23 21:04:08 -0500

    able to resolve a promise

--- src/davePromiseImpl.js
+++ src/davePromiseImpl.js
@@ -4,30 +4,21 @@
 var result;
 var resolved = false;
 
-const impl = function(callback){
+var impl = function(callback){
 
     callback(function(resolvedValue){
         result = resolvedValue;
     });
-
-    //function(resolve) { resolve('hi') }
-    /*
-        function((resolve) => {return value}){
-
-            resolve('hi');
-        }
-     */
 
     if(result){
         resolved = true;
     }
 };
 
-impl.then = function(handler){
+impl.prototype.then = function(handler){
     if(resolved){
         return handler(result);
     }
-
 }
 
 export default impl;
--- test/unit/promise-test.js
+++ test/unit/promise-test.js
@@ -6,7 +6,7 @@ const DavePromise = DavePromiseImpl;
 describe('Promise', () => {
 
     it('should callback when resolve is invoked', () => {
-        DavePromise(resolve => {
+        var testPromise = new DavePromise(resolve => {
             resolve('hi!');
         }).then(value => {
             expect(value).to.equal('hi!');
