32845995bb97f66b8fe3759e302bfe63e13e1c28
dschinkel <dschinkel@gmail.com>
2016-04-24 00:30:40 -0500

    removed unecessary code

--- src/davePromiseImpl.js
+++ src/davePromiseImpl.js
@@ -1,14 +1,17 @@
 
 var result;
+var counter = 0;
 
 var Impl = function(callback){
+
     callback(function(resolvedValue){
         result = resolvedValue;
     });
+
 };
 
-Impl.prototype.then = function(handler){
-    return handler(result);
+Impl.prototype.then = function(callback){
+    callback(result);
 }
 
 export default Impl;
