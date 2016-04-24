6cc1309736b4e8902fd0286c8ae72d3ccfa34e5e
dschinkel <dschinkel@gmail.com>
2016-04-24 00:10:55 -0500

    removed unecessary code but still need 2nd test to await

--- src/davePromiseImpl.js
+++ src/davePromiseImpl.js
@@ -1,25 +1,14 @@
 
-
-
 var result;
-var resolved = false;
 
-var Impl = function(callback){
-
-
+var Impl = (function(callback){
     callback(function(resolvedValue){
         result = resolvedValue;
     });
-
-    if(result){
-        resolved = true;
-    }
-};
+});
 
 Impl.prototype.then = function(handler){
-    if(resolved){
-        return handler(result);
-    }
+    return handler(result);
 }
 
 export default Impl;
--- test/unit/promise-test.js
+++ test/unit/promise-test.js
@@ -7,10 +7,49 @@ const DavePromise = Impl;
 describe('Promise', () => {
 
     it('should callback when resolve is invoked', () => {
-        var testPromise = new DavePromise(resolve => {
+        new DavePromise(resolve => {
             resolve('hi!');
         }).then(value => {
             expect(value).to.equal('hi!');
+        });
+    });
+
+    /*
+        Testing asynchronous code with Mocha could not be simpler! Simply invoke the callback when your test is complete.
+        By adding a callback (usually named done) to it() Mocha will know that it should wait for completion.
+     */
+
+
+    /*
+        Below mocha function is equiv to:
+
+        it('...', function(done){
+
+            done();
+            //call done somewhere in here to let mocha know it can stop waiting
+        };
+
+        resolve => {
+            setTimeout(() => resolve('hi!'), 9000);
+        }
+
+        equiv:
+
+        function(resolve, done){
+            setTimeout(() => resolve('hi', 9000);
+            done()
+        }
+
+     */
+
+
+
+    it('should callback when resolved at a later time', done => {
+        new DavePromise(resolve => {
+            setTimeout(() => resolve('hi!'), 4000);
+        }).then(value => {
+            expect(value).to.equal('hi!');
+            done();
         });
     });
 
