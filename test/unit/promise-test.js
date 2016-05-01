d7019a44c9c573311a91799788328c40db22b2ab
dschinkel <dschinkel@gmail.com>
2016-05-01 16:00:17 -0500

    cleanup

--- test/unit/promise-test.js
+++ test/unit/promise-test.js
@@ -22,16 +22,4 @@ describe('Promise', () => {
     //    });
     //});
 
-    //it('should handle reject correctly', done => {
-    //    var error = new Error('something went horribly wrong because Dave M. Wrote this code!');
-    //
-    //    new DavePromise((resolve, reject) => {
-    //        reject(error);
-    //    }).then(() => {
-    //        throw new Error('This should not be called');
-    //    }, err => {
-    //        expect(err).to.equal(error);
-    //        done();
-    //    });
-    //});
 });
